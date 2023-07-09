4f205b33fe6358d97b194368bfb16dfee1baf3b9
John Wu <yuw385@g.harvard.com>
2023-07-09 11:35:44 -0700

    Adding Header to pages

--- app.py
+++ app.py
@@ -13,30 +13,31 @@ app.debug = True
 @app.route("/")
 def index():
     # return 'Hello, World!'
-    return render_template('index.html',navigation=links)
+    return render_template('index.html',navigation=links, header='Home')
     # return render_template('test.html')
 
 
 @app.route("/home")
 def home():
-    return render_template('index.html',navigation = links)
+    return render_template('index.html',navigation = links, header='Home')
 
 @app.route("/about")
 def about():
-    return render_template('about.html',navigation = links)
+    return render_template('about.html',navigation = links, header='About')
 
 @app.route("/list")
 def list():
-    return render_template('table.html')
+    return render_template('table.html',header = 'Comment Table')
 
 @app.route("/contact", methods = ['GET','POST'])
 def contact():
     if request.method == 'POST':
+        email = request.form.get('name')
         email = request.form.get('email')
         comments = request.form.get('comments')
 
 
-    return render_template('Form.html')
+    return render_template('Form.html', header='Post Your Comments')
 
 # @app.route("/registration")
 # def registration():
